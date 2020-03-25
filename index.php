6172d794f065f2eab9807fc3e9b375f8c6fc5926
guyxlous <62648116+guyxlous@users.noreply.github.com>
2020-03-25 19:15:45 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> 舔狗日记-思源资源网www.isiyuan.net</title>
+		<meta name="keywords" content="彩虹海,www.isiyuan.net">
+        <meta name="description" content="彩虹海，一个努力拼搏的人">
+        <link rel="shortcut icon" href="favicon.ico">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="static/css/main.css" />
+		<noscript><link rel="stylesheet" href="static/css/noscript.css" /></noscript>
+	</head>
+	<body onselectstart="return false" oncontextmenu=self.event.returnValue=false> 
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span>舔狗日记</span>						</div>
+						<div class="content">
+							<div class="inner">
+								<h2><?php echo date("Y年m月d日D")?>   晴</h2>
+								<h3><script src="./api.php "></script></h3>
+							</div>
+						</div>
+						<nav>
+							<!--<ul>
+							
+								<li><a href="./">虚位以待</a></li>
+
+								<li><a href="./">小有成就</a></li>
+								<li><a href="./">虚位以待</a></li>
+								<li><a href="./">虚位以待</a></li>
+								<li><a href="./">虚位以待</a></li>
+
+
+								<li><a href="#elements">Elements</a></li>
+							</ul>
+						</nav>-->
+					</header>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">©MMMMMM 版权所有<a target="_black" href='https://www.isiyuan.net'>思源资源网发布</a></p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="static/js/jquery.min.js"></script>
+			<script src="static/js/skel.min.js"></script>
+			<script src="static/js/util.js"></script>
+			<script src="static/js/main.js"></script>
+ 
+
+	</body>
+</html>
